f902c876b2d27235a240909543dd17d157974349
Marco <marco.cylim@gmail.com>
2017-06-29 13:09:04 +0800

    Updated scripts to read from jquery text instead of data attributes.

--- assets/scripts/sharable-text.js
+++ assets/scripts/sharable-text.js
@@ -4,20 +4,20 @@ var sharableURL = window.location.href;
 var facebookAppID = 'YOUR_APP_ID_HERE';
 
 $(".sharable-text").each(function(){
-	$(this).append('<a class="sharable-text__facebook" href="https://www.facebook.com/sharer/sharer.php?u=' + sharableURL + '" target="_blank">Share this</a> <a class="sharable-text__twitter" href="https://twitter.com/intent/tweet?text=' + $(this).data('sharable-text') + '" target="_blank">Tweet this</a>');
+	$(this).after('<a class="sharable-text__facebook" href="https://www.facebook.com/sharer/sharer.php?u=' + sharableURL + '" target="_blank">Share this</a> <a class="sharable-text__twitter" href="https://twitter.com/intent/tweet?text=' + $(this).text() + '&url=' + sharableURL + '" target="_blank">Tweet this</a>');
 	
 });
 
 
 // Normal sharing
 // $(".sharable-text").each(function(){
-// 	$(this).append('<a class="sharable-text__facebook" href="https://www.facebook.com/sharer/sharer.php?u=' + sharableURL + '" target="_blank">Share this</a> <a class="sharable-text__twitter" href="https://twitter.com/intent/tweet?text=' + $(this).data('sharable-text') + '" target="_blank">Tweet this</a>');
+// 	$(this).after('<a class="sharable-text__facebook" href="https://www.facebook.com/sharer/sharer.php?u=' + sharableURL + '" target="_blank">Share this</a> <a class="sharable-text__twitter" href="https://twitter.com/intent/tweet?text=' + $(this).text() + '&url=' + sharableURL + '" target="_blank">Tweet this</a>');
 // });
 
 
 // Facebook dialog sharing with quotes
 // $(".sharable-text").each(function(){
-// 	$(this).append('<a class="sharable-text__facebook" data-sharable-text="' + $(this).data('sharable-text') + '">Share this</a> <a class="sharable-text__twitter" href="https://twitter.com/intent/tweet?text=' + $(this).data('sharable-text') + '" target="_blank">Tweet this</a>');
+//  $(this).after(' <a class="sharable-text__facebook" data-sharable-text="' + $(this).text() + '"><i class="icon icon-facebook small"></i><span class="sharable-text__tooltip"> Share this quote</span></a> <a class="sharable-text__twitter" href="https://twitter.com/intent/tweet?text=' + $(this).text() + '&url=' + sharableURL + '" target="_blank"><i class="icon icon-twitter small"></i><span class="sharable-text__tooltip"> Share this quote</span></a>');
 // });
 
 // if ( $('.sharable-text').length > 0 ) {
